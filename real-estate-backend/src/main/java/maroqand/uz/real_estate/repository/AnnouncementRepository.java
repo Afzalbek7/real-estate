4cb2a41c74230f1dc9f1a5bb69e54f88c306bdca
Afzalbek7 <afzalmuxamadjonov099@gmail.com>
2021-12-09 10:45:41 +0500

    updated in 09.12.2021

--- real-estate-backend/src/main/java/maroqand/uz/real_estate/repository/AnnouncementRepository.java
+++
@@ -1,7 +0,0 @@
-package maroqand.uz.real_estate.repository;
-
-import maroqand.uz.real_estate.domain.Announcement;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
-}
